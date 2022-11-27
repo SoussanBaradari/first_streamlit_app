402da7eff6871be98c6d04104d8509b3af1b2da5
SoussanBaradari <31281266+SoussanBaradari@users.noreply.github.com>
2022-11-27 15:52:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,3 +70,5 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+steamlit.text("Thanks for adding a new fruit.")
+
