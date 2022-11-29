32cd83427e6db34a428542922b5e732548b13cf2
SoussanBaradari <31281266+SoussanBaradari@users.noreply.github.com>
2022-11-29 07:09:17 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # out put it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-# import sonwflake.connector
+import sonwflake.connector
